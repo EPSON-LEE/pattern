924ce83b3e941386571b20f6c8d79553c647749a
lijiahao <lijiahao@keep.com>
2019-03-25 14:14:05 +0800

    add:chapter1

---
+++ chapter1/index.js
@@ -0,0 +1,87 @@
+/**
+ * duck
+ */
+
+var duck = {
+  duckSing: function() {
+    console.log('嘎嘎嘎')
+  }
+}
+
+var chicken = {
+  duckSing: function() {
+    console.log('嘎嘎噶')
+  }
+}
+
+var choir = []
+
+var joinChoir = function(animal) {
+  if (animal && typeof animal.duckSing === 'function') {
+    choir.push(animal)
+    console.log('恭喜加入合唱团')
+    console.log('合唱团已有成员数量' + choir.length)
+  }
+}
+
+joinChoir(duck)
+joinChoir(chicken)
+
+/**
+ * 多态 polymorphism
+ */
+
+ var makeSound = function(animal) {
+   animal.sound()
+ }
+
+ var Duck = function() {}
+
+ Duck.prototype.sound = function() {
+   console.log('嘎嘎嘎')
+ }
+
+ var Chicken = function() {
+   console.log('咯咯咯')
+ }
+
+ makeSound(new Duck())
+ makeSound(new Chicken())
+
+ /**
+ * 地图
+ */
+
+ var googleMap = {
+   show: function() {
+     console.log('开始渲染google地图')
+   }
+ }
+
+ var baiduMap = {
+  show: function() {
+    console.log('开始渲染baidu地图')
+  }
+}
+
+ var renderMap = function(type) {
+   if (type === 'google') {
+     googleMap.show()
+   } else {
+     baiduMap.show()
+   }
+ }
+
+ renderMap('google')
+ renderMap('baidu')
+
+// 进化
+
+ var renderMap = function(map) {
+   if (map.show instanceof Function) {
+     map.show()
+   }
+ }
+
+ renderMap(googleMap)
+ renderMap(baiduMap)
